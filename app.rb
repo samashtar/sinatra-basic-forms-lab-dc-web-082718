b2abf713bb90a0421fb3098859c4046790378ae4
samashtar <sam.ashtar@flatironschool.com>
2018-09-18 10:42:27 -0400

    lab finish

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do
+erb :index
+end
+
+get '/new' do
+erb :create_puppy
+end
+
+post '/' do
+  @puppyname = params[:name]
+  @puppyage =params[:age]
+  @puppybreed =params[:breed]
+  binding.pry
+  erb :display_puppy
+end
+
+
+
 end
